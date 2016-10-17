c0f284d289211d74aeeeee1dd67e51dc86a32b1b
aamcbee <amelia.mcbee@gmail.com>
2016-10-17 18:18:41 -0500

    Added full sgd implementation and predict functions

--- Solutions/Exercise 4/sparse_sgd_3.py
+++ Solutions/Exercise 4/sparse_sgd_3.py
@@ -68,3 +68,76 @@ def rm_sgd_segment(Data, Index, y, m, b0, step_start, lam=.05):
     b0 *= (1 - 2 * lam * stepsize.sum())
     b0[bindex] /= (1 - 2 * lam * stepsize.sum())
     return b0
+
+@jit
+def sp_rm_sgd(Data, Index, y, m, b0, lam=1e-5, blocksize=1000):
+    '''
+    This implements the stochastic gradient descent algorithm for logistic regression
+    using Robbin's Monro stepsize choice for the entire sparse dataset. Note that the
+    algorithm is implemented in chunks of size "blocksize," which allows us to use
+    rm_sgd_segment on more reasonable chunks of the data.
+
+    Inputs:
+    ----------
+    Note that these inputs are based on a sparse dataset of N samples with P features.
+
+    Data: A list of N arrays, each corresponding to the non-trivial data for each
+        sample. This list (along with Index and y) is generated for the url dataset
+        using preprocessor.py
+    Index: A list of N arrays, each corresponding to the non-trivial indices for each
+        sample.
+    Y: An N-vector that records the label, or "number of successes" for each sample.
+        This array is typically comprised of zeros and ones.
+    m: An N-vector that records the number of trials per sample. This is typically an
+        array of all ones.
+    b0: A P-vector, the initial guess for the logistic regression coefficients. Typically
+        we begin with a vector of all zeros. Note that this vector is consumed during the
+        operation (operated on as a view)
+    lam: A float, the penalty coefficient
+    blocksize: An integer, the size of the blocks on which to operate with rm_sgd_segment
+
+    Returns:
+    ------------
+    b0: The final vector of regression coefficients
+    '''
+    # Find the number of samples and calculate the number of even splits that can be made
+    # for the given blocksize.
+    N = len(Data)
+    splits = int(N / blocksize)
+
+    # Carry out the SGD algorithm on each subsequent chunk of data
+    for i in np.arange(splits):
+        # Get the appropriate indices for this chunk of data
+        j = blocksize * i
+        k = blocksize * (i + 1)
+        # Update the guess vector on the chunk of data using rm_sgd_segment
+        b0 = rm_sgd_segment(Data[j:k], Index[j:k], y[j:k], m[j:k], b0, j, lam=lam)
+
+    # This compensates for any "hangover" should the blocksize not evenly divide N.
+    j = splits * blocksize
+    b0 = rm_sgd_segment(Data[j:N], Index[j:N], y[j:N], m[j:N], b0, j, lam=lam)
+    return b0
+
+def predict(D, I, y, b0):
+    '''
+    This function makes binomial predictions for a set of samples and returns
+    the accuracy of the predictions.
+    Inputs:
+    ---------
+    D: A list of the N nontrivial data arrays for each testing sample respectivly
+    I: A list of the N index arrays corresponding to the nontrivial data for each sample
+        respectively.
+    y: An N array of the actual number of "successes per sample"
+    b0: A P array of regression coefficients
+
+    Returns:
+    --------
+    Accuracy: A float value of the the percentage of correct predictions, on a 0-100 scale
+    '''
+    n = len(D)
+    Predict = np.zeros(n)
+    for i in xrange(n):
+        Predict[i] = expit(np.dot(D[i], b0[I[i]]))
+    Predict = (Predict > .5).astype(float)
+    Accuracy = (Predict == y).sum() / float(n) * 100
+    return Accuracy
