b024878283543f67e87f09c4d7b73cc16ffaaf2d
aamcbee <amelia.mcbee@gmail.com>
2016-10-18 16:49:18 -0500

    Added adagrad method to basic sgd_methods.py

--- Solutions/Exercise 4/sgd_methods.py
+++ Solutions/Exercise 4/sgd_methods.py
@@ -297,3 +297,76 @@ def ls_sgd(X, y, m, b0, iters=10, tol=1e-3, check_full=False, smooth=.05, batchs
                     return b1, betas, ll, full
     betas.append(b1)
     return b1, betas, ll, full
+
+def adagrad(X, y, m, b0, iters=10, tol=1e-3, check_full=False, smooth=.05, stepsize = .01):
+    '''
+    Runs the stochastic gradient descent algorithm for the negative log likelihood
+    function of a given dataset using the adagrad method and exponentially
+    weighted moving average convergence criterion.
+
+    Inputs:
+    ----------
+    X: N x P array of samples
+    y: N vector of the number of successes per sample
+    m: N vector of the number of trials per sample
+    b0: P vector corresponding to the initial guess vector
+    iters: integer, optional argument set to maximum number of times to run through
+        the sample set X, set to 10
+    tol: float, optional argument for the error bound used to determine convergence.
+    check_full: boolean, optional argument to determine whether to calculate the full
+        negative log likelihood at each step
+    ----------
+    Outputs:
+    ----------
+    b1: P vector: The final solved for vector that "minimizes" the negative log-likelihood
+    betas: A list of the beta values stored at each iteration.
+    ll: A list of the exponentially weighted moving average of the nll contribution at each
+        iteration
+    full: If check_full == True, then this is a list of the full negative log likelihood at
+        each iteration. If not, this simply returns an empty set
+
+    '''
+
+    # Initialize various values
+    ll = []
+    full = []
+    betas = []
+    N, P = X.shape
+    index = np.arange(N)
+    G = np.zeros(P) # Diagonal of G, "hessian approximation"
+    for i in np.arange(iters):
+        np.random.shuffle(index)
+        for j in np.arange(N):
+            k = index[j]
+
+            # This ensures the first computed nll contribution is in place
+            if i==0 and j==0:
+                ll.append(nll(X[k], y[k], m[k], b0))
+
+            # This computes the full log-likelihood for the entire dataset at the
+            # b point
+            if check_full:
+                full.append(full_nll(X, y, m, b0))
+
+            betas.append(b0)
+            
+            # Calculate the gradient contribution
+            grad_i = gi(X[k], y[k], m[k], b0)
+            # Update G
+            G += grad_i**2
+            # Compute the next beta iteration
+            b1 = b0 - stepsize * np.sqrt(G)**(-1) * grad_i
+            llb1 = smooth * nll(X[k], y[k], m[k], b1) + (1-smooth) * ll[-1]
+            ll.append(llb1)
+
+            # This allows for a burn in period because convergence is measured using the EWMA
+            if j < N * .01:
+                b0 =b1
+            else:
+                if np.abs(ll[-1] - ll[-2]) > tol:
+                    b0 = b1
+                else:
+                    betas.append(b1)
+                    return b1, betas, ll, full
+    betas.append(b1)
+    return b1, betas, ll, full
